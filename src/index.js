0e7683184ad829ec84cf7b921bc2777f6bcc3ec1
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2023-03-29 11:18:31 +0200

    update config file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
+const http = require('http');
 const app = express();
 const port = process.env.PORT || 8000;
 const connectDB = require('./config/mongo.config');
